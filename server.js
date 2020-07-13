b6a6af6afa83669937b8f732b627737d489a25dd
robertmoore40 <robertcmoore92@gmail.com>
2020-07-13 11:50:34 -0400

    added additional logic

---
+++ server.js
@@ -0,0 +1,16 @@
+import { listenAndServe } from 'https://deno.land/std/http/server.ts';
+import { acceptWebSocket, acceptable} from 'https://deno.land/std/ws/mod.ts';
+import chat from './chat.js'
+
+listenAndServe({port:3000}, async req => {
+
+    if (acceptable(req)) {
+        acceptWebSocket({
+             conn: req.conn,
+             bufReader: req.r,
+             bufWriter: req.w,
+             headers: req.headers
+        }).then(chat)
+    }
+
+})
