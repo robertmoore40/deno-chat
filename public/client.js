c67370d2b88a2b178d934239f4c5478c7d921cbe
robertmoore40 <robertcmoore92@gmail.com>
2020-07-13 11:59:30 -0400

    added splits for queries

--- public/client.js
+++ public/client.js
@@ -4,7 +4,13 @@ window.addEventListener('DOMContentLoaded', () => {
     ws = new WebSocket('ws://localhost:3000/ws');
     ws.addEventListener('open', OnConnectionOpen);
     ws.addEventListener('open', onMessageReceived);
+
+    const queryParams = getQueryParams()
+
+
 })
+
+
 
 function OnConnectionOpen() {
     console.log('Connection Open');
@@ -12,4 +18,13 @@ function OnConnectionOpen() {
 
 function onMessageReceived(){
     console.log('New Message Received', event);
+}
+
+function getQueryParams(){
+    const search = window.location.search.substring(1);
+    const pairs = search.split('&');
+    for (const pair of pairs) {
+        const parts = pair.split('=');
+
+    }
 }
