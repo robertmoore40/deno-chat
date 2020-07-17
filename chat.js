4f4c57d08b54fef0d25c8fe99c5bcecf9fd19553
robertmoore40 <robertcmoore92@gmail.com>
2020-07-17 08:51:47 -0400

    added logic

--- chat.js
+++ chat.js
@@ -28,6 +28,66 @@ const usersMap = new Map();
 
 const groupsMap = new Map();
 
+export default async function chat(ws) {
+
+  const userId = v4.generate();
+
+
+  for await (let data of ws) {
+    const event = typeof data === "string" ? JSON.parse(data) : data;
+
+
+    if (isWebSocketCloseEvent(data)) {
+
+      leaveGroup(userId);
+      break;
+    }
+
+    let userObj;
+
+    switch (event.event) {
+
+      case "join":
+
+        userObj = {
+          userId,
+          name: event.name,
+          groupName: event.groupName,
+          ws,
+        };
+
+
+        usersMap.set(userId, userObj);
+
+
+        const users = groupsMap.get(event.groupName) || [];
+        users.push(userObj);
+        groupsMap.set(event.groupName, users);
+
+
+        emitUserList(event.groupName);
+
+        emitPreviousMessages(event.groupName, ws);
+        break;
+
+      case "message":
+        userObj = usersMap.get(userId);
+        const message = {
+          userId,
+          name: userObj.name,
+          message: event.data,
+        };
+        const messages = messagesMap.get(userObj.groupName) || [];
+        messages.push(message);
+        messagesMap.set(userObj.groupName, messages);
+        emitMessage(userObj.groupName, message, userId);
+        break;
+    }
+  }
+}
+
+
+
 /**
  * groupName: [message1,message2]
  *
